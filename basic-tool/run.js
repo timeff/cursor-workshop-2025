6881c4955c0755520ed8ad1045ad000fcb1d80cd
Time Kawin <time@finnomena.com>
2025-02-22 16:48:33 +0800

    Update README with console runner documentation
    - Added description of `run.js` in readme.MD
    - Removed `basic-tool/console-run.js` file
    - Updated README to clarify test suite running instructions

---
+++ basic-tool/run.js
@@ -0,0 +1,39 @@
+// const { ex1 } = require("./prompt-bar.js");
+// console.log("prompt-bar.js - ex1");
+// console.log(
+//   ex1([
+//     { name: "Adam", age: 25 },
+//     { name: "Jane", age: 30 },
+//     { name: "Bryan", age: 25 },
+//     { name: "Jill", age: 34 },
+//   ])
+// );
+
+// const { ex2 } = require("./prompt-bar.js");
+// console.log("prompt-bar.js - ex2");
+// console.log(ex2(["a", "a", "a", "b", "b", "c"]));
+
+// const { countVowels } = require("./chat.js");
+// console.log("chat.js - countVowels");
+// console.log(countVowels("hello"));
+
+// const { allocateAndFormatThaiBaht } = require("./chat.js");
+// console.log("chat.js - allocateAndFormatThaiBaht");
+// console.log(allocateAndFormatThaiBaht(1000, 3));
+
+// const { getLatestPTTPrice } = require("./chat.js");
+// getLatestPTTPrice().then((price) => {
+//   console.log("chat.js - getLatestPTTPrice");
+//   console.log(price);
+//   console.log("--------------------------------");
+// });
+
+// const { summarizeArticle } = require("./chat.js");
+// summarizeArticle(
+//   "https://www.theverge.com/elon-musk/617427/musk-trump-doge-recession-unemployment",
+//   250
+// ).then((summary) => {
+//   console.log("chat.js - summarizeArticle");
+//   console.log(summary);
+//   console.log("--------------------------------");
+// });
